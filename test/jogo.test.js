95c8fb12227d79f4581932f02cd22dfad951dbd7
charles <crramiress@gmail.com>
2021-06-10 11:18:49 -0300

    Update jogo.test.js and add coverage

--- test/jogo.test.js
+++ test/jogo.test.js
@@ -18,6 +18,8 @@ beforeEach(() => {
 })
 
 
+// STATUS 200
+
 test('Deve ser possivel listar jogos', async () => {
     await request.get('/jogos').expect(200)
 })
@@ -36,3 +38,19 @@ test('Deve ser possivel deletar um jogo', async () => {
 test('Deve ser possivel atualizar a plataforma de um jogo', async () => {
     await request.put('/jogos/1').send({plataforma: "Xbox"}).expect(200);
 })
+
+
+// STATUS 400
+
+test('Não deve adicionar jogo com erro de cadastro.', async () => {
+    await request.post('/jogos').send({nome: "The Witcher III", plataforma: "PC", valor: 120}).expect(400);
+})
+
+test('Não deve ser possível deletar um jogo inexistente.', async () => {
+    await request.delete('/jogos/10').expect(400);
+})
+
+test('Não deve ser possível atualizar um jogo inexistente', async () => {
+    await request.put('/jogos/10').send({plataforma: "xbox"}).expect(400);
+})
+
